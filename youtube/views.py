083206da57e110b6715e8aac4596feb40fa4b942
kelvzzy <kelvinworldstudy@yahoo.com>
2023-09-21 05:01:28 -0700

    front implementation

--- youtube/views.py
+++ youtube/views.py
@@ -1,12 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from pytube import YouTube
 
 # Create your views here.
 def youtube(request):
     link2=None
+    yt_title=None
     if request.method=="POST":
         link2=request.POST.get("link")
-
+        try:
+            yt = YouTube(link2)
+            yt_title = yt.title  # Get the title of the YouTube video
+        except Exception as e:
+            # Handle any exceptions that may occur (e.g., invalid URL)
+            print(f"Error: {e}")
     return render(request, "youtube/index.html", 
-                  {"link":link2}
+                  {"link":yt_title}
                   )
